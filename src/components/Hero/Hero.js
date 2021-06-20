98fcf098cfed3fbb277a840273e6795698d4c754
sujithchenanath909 <sujithchenanath@gmail.com>
2021-06-20 21:14:34 +0530

    upddated about

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import Link from 'next/link';
 import { Section, SectionText, SectionTitle } from '../../styles/GlobalComponents';
 import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
